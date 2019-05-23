a2de705394599d5876c4a32819bfb85a80012637
johnniang <johnniang@riseup.net>
2019-05-24 06:30:59 +0800

    Refactor comment api

--- src/apis/comment.js
+++ src/apis/comment.js
@@ -5,24 +5,11 @@ const baseUrl = '/api/content'
 const commentApi = {}
 
 /**
- * Lists comment.
- * @param {String} target
- * @param {Object} view
- */
-function listComment(target, targetId, view, pagination) {
-  return service({
-    url: `${baseUrl}/${target}/${targetId}/comments/${view}`,
-    params: pagination,
-    method: 'get'
-  })
-}
-
-/**
  * Creates a comment.
  * @param {String} target
  * @param {Object} comment
  */
-function createComment(target, comment) {
+commentApi.createComment = (target, comment) => {
   return service({
     url: `${baseUrl}/${target}/comments`,
     method: 'post',
@@ -31,41 +18,12 @@ function createComment(target, comment) {
 }
 
 // List api
-
-commentApi.listPostComment = (postId, pagination, view = 'list_view') => {
-  return listComment('posts', postId, view, pagination)
-}
-commentApi.listSheetComment = (postId, pagination, view = 'list_view') => {
-  return listComment('sheets', postId, view, pagination)
-}
-commentApi.listJournalComment = (postId, pagination, view = 'list_view') => {
-  return listComment('journals', postId, view, pagination)
-}
-
-// Creation api
-
-commentApi.createPostComment = comment => {
-  return createComment('posts', comment)
-}
-
-commentApi.createSheetComment = comment => {
-  return createComment('sheets', comment)
-}
-
-commentApi.createJournalComment = comment => {
-  return createComment('journals', comment)
-}
-
-commentApi.createComment = (comment, type) => {
-  if (type == 'sheet') {
-    return commentApi.createSheetComment(comment)
-  }
-
-  if (type == 'journal') {
-    return commentApi.createJournalComment(comment)
-  }
-
-  return commentApi.createPostComment(comment)
+commentApi.listComments = (target, targetId, view = 'list_view', pagination) => {
+  return service({
+    url: `${baseUrl}/${target}/${targetId}/comments/${view}`,
+    params: pagination,
+    method: 'get'
+  })
 }
 
 export default commentApi
